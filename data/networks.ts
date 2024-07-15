eaf5230d5e2e65787469a051c4256cad157c1996
SantiagoPittella <pittellasantiago@gmail.com>
2024-07-15 18:41:28 +0200

    add shyft network

--- data/networks.ts
+++ data/networks.ts
@@ -67,38 +67,29 @@ export const dockerizedNode: ZkSyncNetwork = {
 
 const publicChains: ZkSyncNetwork[] = [
   {
-    id: 324,
-    key: "mainnet",
-    name: "zkSync",
-    rpcUrl: "https://mainnet.era.zksync.io",
-    blockExplorerUrl: "https://era.zksync.network",
-    blockExplorerApi: "https://block-explorer-api.mainnet.zksync.io",
-    displaySettings: {
-      showPartnerLinks: true,
-    },
-    l1Network: l1Networks.mainnet,
-  },
-  {
-    id: 300,
-    key: "sepolia",
-    name: "zkSync Sepolia Testnet",
-    rpcUrl: "https://sepolia.era.zksync.dev",
-    blockExplorerUrl: "https://sepolia-era.zksync.network",
-    blockExplorerApi: "https://block-explorer-api.sepolia.zksync.dev",
-    displaySettings: {
-      showPartnerLinks: true,
-    },
-    l1Network: l1Networks.sepolia,
-  },
-  {
     id: 270,
-    key: "stage",
-    name: "zkSync Stage",
-    rpcUrl: "https://z2-dev-api.zksync.dev",
-    blockExplorerUrl: "https://sepolia-beta.staging-scan-v2.zksync.dev",
-    blockExplorerApi: "https://block-explorer-api.stage.zksync.dev",
-    l1Network: l1Networks.sepolia,
-    hidden: true,
+    key: "shyft-devnet",
+    name: "Shyft Devnet",
+    rpcUrl: "https://dev.rpc.sepolia.shyft.lambdaclass.com",
+    blockExplorerApi: "http://35.204.251.135:3020",
+    blockExplorerUrl: "https://k8s.explorer.sepolia.shyft.lambdaclass.com",
+    l1Network: {
+      id: 11155111,
+      name: "Sepolia",
+      nativeCurrency: {
+        name: "Ether",
+        symbol: "ETH",
+        decimals: 18,
+      },
+      rpcUrls: {
+        default: {
+          http: ["http://eth-sepolia"],
+        },
+        public: {
+          http: ["http://eth-sepolia"],
+        },
+      },
+    },
   },
 ];
 
